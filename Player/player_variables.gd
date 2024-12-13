2601d5b32ff4ee3b3de3cbdd7336dcd6ac7e4c4e
skeeth <ethanhalbe@gmail.com>
2024-12-13 16:38:07 -0600

    Combat visuals with scene transitions
    includes both a shader transition that i think is kinda nauseating and i spent way too long trying to make look better as well as a simple one, toggleable with a property in sack_world script

--- Player/player_variables.gd
+++ Player/player_variables.gd
@@ -3,11 +3,12 @@ extends Node
 signal health_changed(new)
 signal new_item(title,desc,flavor,image)
 #shouldn't really be in this script but this is the only autoload for now
+signal combat_fading
 signal combat_ended 
 
 
 var sack_npc_type_counts = {}
-#var previous_position:Vector2
+
 var current_health:float = 100:
 	set(v):
 		current_health = v
@@ -17,8 +18,8 @@ var max_hp:float = 100:
 		max_hp = v
 		health_changed.emit(current_health)
 var sack_heal:float = 20
-var cheer_rate:float = 3
-var survival_time:float = 20
+var cheer_rate:float = 1
+var survival_time:float = 25
 var walkspeed:float = 200
 var dash_multiplier:float = 4
 
@@ -30,3 +31,5 @@ var spin_scale:float = 1.0
 var quick_stun:float = 0.6
 var quick_kb:float = 300
 var quick_scale:float = 1.0
+
+var outside_movespeed:float = 300
--- SpiritBar.gd
+++ SpiritBar.gd
@@ -1,8 +1,11 @@
 extends TextureProgressBar
 
 @export var margin:float
+@export var krampus:Krampus
 @onready var combat_scene = preload("res://sack_world.tscn")
 @export var outside_scene:Node2D
+@export var out_cam: Camera2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#position.x = get_viewport_rect().size.x/2.0/get_parent().zoom.x
@@ -14,17 +17,32 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	value -= delta * PlayerVariables.cheer_rate * 60.0
+	if process_mode == Node.PROCESS_MODE_ALWAYS:
+		return
 	if value <= 0.0:
 		#could do some kind of transition
+		process_mode = Node.PROCESS_MODE_ALWAYS
 		outside_scene.process_mode = Node.PROCESS_MODE_DISABLED
-		outside_scene.visible = false
+		#outside_scene.visible = false
 		var combat_instance = combat_scene.instantiate() 
+		combat_instance.position = krampus.global_position
+		combat_instance.target = krampus
 		get_tree().root.add_child(combat_instance)
+		var shrink = create_tween()
+		shrink.tween_property(krampus,"scale",Vector2(0.2,0.2),0.3)
 		visible = false
-		await PlayerVariables.combat_ended
+		await PlayerVariables.combat_fading
+		out_cam.make_current()
 		visible = true
 		value = max_value
 		outside_scene.visible = true
+		krampus.process_mode = Node.PROCESS_MODE_WHEN_PAUSED
+		await  PlayerVariables.combat_ended
+		var grow = create_tween()
+		grow.tween_property(krampus,"scale",Vector2.ONE,1.5);
+		#await grow.finished
+		krampus.process_mode = Node.PROCESS_MODE_INHERIT
+		process_mode = Node.PROCESS_MODE_INHERIT
 		outside_scene.process_mode = Node.PROCESS_MODE_INHERIT
 
 	pass
